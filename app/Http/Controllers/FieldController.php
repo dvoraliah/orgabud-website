25c456c62bb8a9abcd83ee4b3d871544597563c3
dvoraliah <dvoraliah@gmail.com>
2022-02-17 14:08:12 +0100

    first commit

---
+++ app/Http/Controllers/FieldController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Field;
+use Illuminate\Http\Request;
+
+class FieldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Field  $field
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Field $field)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Field  $field
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Field $field)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Field  $field
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Field $field)
+    {
+        //
+    }
+}
