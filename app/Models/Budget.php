b87f41a891ca57430dfa5ef7bc426280d0f568dc
Dvoraliah <dvoraliah@gmail.com>
2022-10-25 17:38:32 +0200

    nettoyage du model budget

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -3,19 +3,22 @@
 namespace App\Models;
 
 use App\Models\User;
-use App\Models\Year;
 use App\Models\Field;
-use App\Models\Month;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Budget extends Model
 {
     use HasFactory;
-
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = ['value', 'field_id', 'user_id', 'month', 'year', 'is_debited'];
 
     /**
@@ -40,27 +43,5 @@ class Budget extends Model
         return $this->belongsTo(User::class);
     }
     
-    /**
-     * Month
-     * Un budget appartient à un mois
-     *
-     * @return BelongsTo
-     */
-    // public function month(): BelongsTo
-    // {
-    //     return $this->belongsTo(Month::class);
-    // }
-
-
-    // /**
-    //  * Year
-    //  * Un budget appartient à une année
-    //  *
-    //  * @return BelongsTo
-    //  */
-    // public function year(): BelongsTo
-    // {
-    //     return $this->belongsTo(Year::class);
-    // }
 }
 
